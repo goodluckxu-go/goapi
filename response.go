729502995ca678e4bdb23431f565bb9faa90c6bf
xurui <xurui@xingqiwan.com>
2024-07-29 17:10:18 +0800

    feat: 增加SSEResponse返回

--- response.go
+++ response.go
@@ -1,10 +1,12 @@
 package goapi
 
 import (
+	"bytes"
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type Response interface {
@@ -107,4 +109,56 @@ func (h *FileResponse) Write(w http.ResponseWriter) {
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%v\"", h.Filename))
 	w.WriteHeader(200)
 	_, _ = w.Write(h.Body)
+}
+
+type SSEvent struct {
+	w http.ResponseWriter
+}
+
+type SSEventData struct {
+	Event string
+	Data  string
+	Id    string
+	Retry uint
+}
+
+func (s *SSEvent) Write(data SSEventData) {
+	var buf bytes.Buffer
+	if data.Event != "" {
+		buf.WriteString("event: " + data.Event + "\n\n")
+	}
+	buf.WriteString("data: " + data.Data + "\n\n")
+	if data.Id != "" {
+		buf.WriteString("id: " + data.Id + "\n\n")
+	}
+	if data.Retry > 0 {
+		buf.WriteString("retry: " + strconv.Itoa(int(data.Retry)) + "\n\n")
+	}
+	_, _ = s.w.Write(buf.Bytes())
+	if f, ok := s.w.(http.Flusher); ok {
+		f.Flush()
+	}
+}
+
+type SSEResponse struct {
+	SSEWriter func(s *SSEvent)
+}
+
+func (s *SSEResponse) GetBody() any {
+	return ""
+}
+
+func (s *SSEResponse) GetContentType() string {
+	return "text/event-stream"
+}
+
+func (s *SSEResponse) SetContentType(contentType string) {
+}
+
+func (s *SSEResponse) Write(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", s.GetContentType())
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.WriteHeader(200)
+	s.SSEWriter(&SSEvent{w: w})
 }
