e6967c05ee3f0b453b48f8bca8317e4612fac044
xurui <xurui@xingqiwan.com>
2026-06-29 15:01:27 +0800

    fix: debug.go打印方法和路由统计

--- debug.go
+++ debug.go
@@ -2,6 +2,7 @@ package goapi
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -16,6 +17,9 @@ func debugPrintRouter(log Logger, paths []*pathInfo) {
 	maxMethodLen := len(headMethod)
 	maxPathLen := len(headPath)
 	maxPosLen := len(headPos)
+	methodMap := map[string]int{}
+	countRouter := 0
+	var methods []string
 	for _, path := range paths {
 		methodLen := len(strings.Join(path.methods, ","))
 		if methodLen > maxMethodLen {
@@ -32,21 +36,66 @@ func debugPrintRouter(log Logger, paths []*pathInfo) {
 		if posLen > maxPosLen {
 			maxPosLen = posLen
 		}
-	}
-	log.Debug(strings.Repeat("-", maxMethodLen+maxPathLen+maxPosLen+10))
-	log.Debug("| %v | %v | %v |", spanFill(headMethod, len(headMethod), maxMethodLen),
-		spanFill(headPath, len(headPath), maxPathLen), spanFill(headPos, len(headPos), maxPosLen))
-	for _, path := range paths {
-		method := strings.Join(path.methods, ",")
-		p := strings.Join(path.paths, ",")
-		if path.isSwagger {
-			p = path.paths[0]
+		for _, method := range path.methods {
+			if methodMap[method] == 0 {
+				methods = append(methods, method)
+			}
+			methodMap[method] += len(path.paths)
+			countRouter += len(path.paths)
 		}
-		pos := debugPos(path)
-		log.Debug("| %v | %v | %v |", spanFill(method, len(method), maxMethodLen),
-			spanFill(p, len(p), maxPathLen), spanFill(pos, len(pos), maxPosLen))
 	}
-	log.Debug(strings.Repeat("-", maxMethodLen+maxPathLen+maxPosLen+10))
+	if countRouter > 0 {
+		lineLen := maxMethodLen + maxPathLen + maxPosLen + 10
+		log.Debug(strings.Repeat("-", lineLen))
+		log.Debug("| %v | %v | %v |", spanFill(headMethod, len(headMethod), maxMethodLen),
+			spanFill(headPath, len(headPath), maxPathLen), spanFill(headPos, len(headPos), maxPosLen))
+		for _, path := range paths {
+			method := strings.Join(path.methods, ",")
+			p := strings.Join(path.paths, ",")
+			if path.isSwagger {
+				p = path.paths[0]
+			}
+			pos := debugPos(path)
+			log.Debug("| %v | %v | %v |", spanFill(method, len(method), maxMethodLen),
+				spanFill(p, len(p), maxPathLen), spanFill(pos, len(pos), maxPosLen))
+		}
+		log.Debug(strings.Repeat("-", lineLen))
+		var countMethods []string
+		for _, method := range methods {
+			newMethod := method + "(" + strconv.Itoa(methodMap[method]) + ")"
+			countMethods = append(countMethods, newMethod)
+		}
+		debugPrintMethod(log, countMethods, countRouter, lineLen)
+	} else {
+		log.Debug("No routing available")
+	}
+}
+
+func debugPrintMethod(log Logger, methods []string, countRouter, totalLen int) {
+	leftLen := 15
+	rightLen := totalLen - leftLen - 2
+	val := ""
+	methodCount := 0
+	for index, method := range methods {
+		if index < len(methods)-1 {
+			method += ", "
+		}
+		if len(val+method) <= rightLen {
+			val += method
+			continue
+		}
+		if methodCount == 0 {
+			log.Debug("| MethodCount: " + spanFill(val, len(val), rightLen) + " |")
+		} else {
+			log.Debug(spanFill("| ", 2, leftLen) + spanFill(val, len(val), rightLen) + " |")
+		}
+		val = method
+		methodCount++
+	}
+	log.Debug(spanFill("| ", 2, leftLen) + spanFill(val, len(val), rightLen) + " |")
+	routerCount := strconv.Itoa(countRouter)
+	log.Debug("| RouterCount: " + spanFill(routerCount, len(routerCount), rightLen) + " |")
+	log.Debug(strings.Repeat("-", totalLen))
 }
 
 func debugPos(path *pathInfo) string {
